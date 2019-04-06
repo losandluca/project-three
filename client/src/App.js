5a76fe4e36aa7f93b2437d182faf118552dcfa90
edgar <GrrrRocks@gmail.com>
2019-04-06 13:12:41 -0700

    completed battle page route

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,25 @@
-import React, { Component } from 'react';
-import Score from "./components/scoreBar";
-import Stage from "./components/playingField";
-import Hand from "./components/hand";
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Battle from "./pages/battle";
+
 import './App.css';
 
-class App extends Component {
-
-  // state = {
-  // cards = [],
-  // player1Health = 1000,
-  // player2Health = 1000
-  // timer = 3:00
-  // };
 
 
-  render() {
+function App() {
     return (
+    <Router>
       <div className="App">
         <h1>Battle Page</h1>
           <hr></hr>    
         <div className="container">
-          <Score />
             <br></br>
-          <Stage /> 
-            {/* <br></br> */}
-          <Hand />
+        {/* <Route exact path="/lobby" component={Lobby} /> */}
+        <Route exact path="/battle" component={Battle} />
         </div>
       </div>
+    </Router>   
     );
-  }
 }
 
 export default App;
---
+++ client/src/pages/battle.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import Score from "../components/scoreBar";
+import Stage from "../components/playingField";
+import Hand from "../components/hand";
+
+
+class Battle extends Component {
+
+    // state = {
+    // cards = [],
+    // player1Health = 1000,
+    // player2Health = 1000
+    // timer = 3:00
+    // }; 
+
+    render() {
+        return (
+            <div>
+                <Score />
+                    <br></br>
+                <Stage />
+                <Hand />
+            </div>
+        )
+    }
+}
+
+export default Battle;
