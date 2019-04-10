179696dceadfc5f6a04ef866543e943f2d3ece2b
Luis Vega <vega.louie91@gmail.com>
2019-04-09 20:39:50 -0700

    no node-modules

--- client/src/components/jumbotron/jumbotron.js
+++ client/src/components/jumbotron/jumbotron.js
@@ -9,7 +9,10 @@ function Jumbotron () {
         <div class="container">
           <h1 class="display-4">Friend Zone.</h1>
           <p class="lead">This is the place where souls go and never comeback. 
-          In the Friend Zone you only have a couple.  
+            In the Friend Zone will have the ability to battle friends in a card
+            them battle to the death. You will be able to invite your friends via Facebook , Gmail,
+            or request friends to join after you create a player card through our survey
+            and sign up method. Best of luck and Battle On! 
           </p>
         </div>
       </div>
