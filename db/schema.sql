22333418fa85a8dda0ed8c59072b4070cf252b80
edgar <GrrrRocks@gmail.com>
2019-04-11 21:14:24 -0700

    finished styling of pages

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE friend_zone;
 
 USE friend_zone;
 
-CREATE TABLE alliance(
+CREATE TABLE alliance( <!--- this is the whole deck>
   alliance_id INT NOT NULL AUTO_INCREMENT,
   user_id INT NOT NULL,
   alliance_member VARCHAR(255) NOT NULL,
@@ -25,4 +25,22 @@ CREATE TABLE battle(
   player_turn_id INT NOT NULL,
   FOREIGN KEY (alliance_id) references alliance (alliance_id),
   PRIMARY KEY (battle_id)
+);
+
+CREATE TABLE users(
+  user_id INT NOT NULL AUTO_INCREMENT,
+  user_name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  image BLOB NOT NULL,
+  PRIMARY KEY (user_id)
+);
+
+CREATE TABLE results(
+  results_id INT NOT NULL AUTO_INCREMENT,
+  foreign key (user_id) references users(user_id),
+  foreign key (battle_id) references battle(battle_id),
+  outcome BIT NOT NULL,
+  score INT NOT NULL,
+  PRIMARY KEY (results_id)
 );
