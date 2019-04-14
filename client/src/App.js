a01219d5b99b27357a160915d9a135cee3bcb707
edgar <GrrrRocks@gmail.com>
2019-04-14 13:56:49 -0700

    navbar now react component

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,25 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Battle from "./pages/battle";
 import Lobby from "./pages/lobby";
 import Home from "./pages/home";
-import './App.css';
 import Register from './pages/register';
+import Navbar from "./components/navbar";
 import defaultCards from "./image.json";
+import './App.css';
 
 
 function App() {
 
   return (
   <Router>
-    <div className="App"> 
-      <div className="container-fluid app">
-        <Route exact path="/" component={Home} />
-        <Route exact path="/lobby" component={Lobby} />
-        <Route exact path="/battle" component={Battle} />
-        <Route exact path="/register" component={Register} />
+    <Navbar />
+      <div className="App"> 
+        <div className="container-fluid app">
+          <Route exact path="/" component={Home} />
+          <Route exact path="/lobby" component={Lobby} />
+          <Route exact path="/battle" component={Battle} />
+          <Route exact path="/register" component={Register} />
+        </div>
       </div>
-    </div>
   </Router>   
   );
 }
