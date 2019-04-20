05ca81607579cc3f441d5d840bccc791f816171f
edgar <GrrrRocks@gmail.com>
2019-04-20 12:06:37 -0700

    to start deploy

--- server.js
+++ server.js
@@ -17,12 +17,14 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 //
 // Requiring our routes
+app.use(routes)
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 //
