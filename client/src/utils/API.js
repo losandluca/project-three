4ab203edfec9e19ac8da73dd80a1d6839ae4fc08
edgar <GrrrRocks@gmail.com>
2019-04-21 12:17:22 -0700

    before merge with new1

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export default {
+
   saveUser: function(userData) {
     return axios.post("/api/signup", userData);
   }
+  
 };
--- server.js
+++ server.js
@@ -8,10 +8,12 @@ var session = require("express-session");
 // Requiring passport as we've configured it
 var passport = require("./config/passport");
 //
+
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 3001;
 var db = require("./models");
 //
+
 // Creating express app and configuring middleware needed for authentication
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,11 +25,13 @@ app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true 
 app.use(passport.initialize());
 app.use(passport.session());
 //
+
 // Requiring our routes
-app.use(routes)
+// app.use(routes)
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 //
+
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
