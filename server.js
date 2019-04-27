6a9b0790683826a4e0f5a2cf2909a111a3a3c2c4
acerise <42525915+acerise85@users.noreply.github.com>
2019-04-27 13:49:35 -0700

    up to date code and starting firebase sync for multi player

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(routes)
 // require("./routes/api/api-routes.js")(app);
 
 // Syncing our database and logging a message to the user upon success
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force:true}).then(function() {
   app.listen(PORT, function() {
     console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
